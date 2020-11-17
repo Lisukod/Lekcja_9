6ea82f8774589ac165c4ab7c38abc2713d681643
Lisukod <45397160+Lisukod@users.noreply.github.com>
2020-11-17 02:48:55 +0100

    Dodano walidacje

--- reader.py
+++ reader.py
@@ -1,10 +1,24 @@
 from sys import argv
+import os
 import csv
 
 src = argv[1]
 dst = argv[2]
 changeList = []
 csvList = []
+
+if not os.path.exists("./csv_origin/{}".format(src)):
+    print("Błąd. Podana ścieżka nie istnieje")
+    exit()
+elif not os.path.isfile("./csv_origin/{}".format(src)):
+    print("Błąd. Podana ścieżka nie jest plikiem.")
+    onlyfiles = [
+        f
+        for f in os.listdir("./csv_origin/")
+        if os.path.isfile(os.path.join("./csv_origin/", f))
+    ]
+    print("Pliki w katalogu:\n{}".format(onlyfiles))
+    exit()
 
 
 with open("csv_origin/{}".format(src), "r") as csv_file:
